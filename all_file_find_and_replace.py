afdcb21cd370aa4d6f4495a3758e86c1fa17863b
mrko <nahsuhk1111@gmail.com>
2023-10-20 15:54:40 +0545

    a

--- all_file_find_and_replace.py
+++ all_file_find_and_replace.py
@@ -2,13 +2,13 @@ import os
 import fileinput
 
 # Define the directory where you want to search for files
-directory = 'C:/Users/kairn/Music/khusbunotes/'
+directory = 'C:/Users/kairn/Music/kk/'
 
 # Function to search and replace in a single file
 def replace_in_file(file_path):
     with fileinput.FileInput(file_path, inplace=True) as file:
         for line in file:
-            print(line.replace('"images/2.png', '"/images/2.png'), end='')
+            print(line.replace('<meta charset="utf-8">', '<meta charset="utf-8">\n<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7033703888764854" crossorigin="anonymous"></script>'), end='')
 
 # Recursively search for files in the directory
 for foldername, subfolders, filenames in os.walk(directory):
